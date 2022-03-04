e18ed6a0fff4f05a73b9e6ce0cf0ad5dabd13f46
arpitacn <arpita@capitalnnumbers.com>
2022-03-04 07:15:27 +0530

    Deleted unwanted file

--- api.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const mongoose = require("mongoose");
-
-router.get("/",(req,res)=>{
-    res.send("Hello world");
-})
-
-module.exports = router;
